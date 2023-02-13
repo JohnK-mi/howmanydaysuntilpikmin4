efc1fc7330faa5274506002673294cc48a6c0173
John Kirchner <j_kirchner@ucsb.edu>
2023-02-12 21:05:10 -0800

    sass support and basic color palette added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,10 +9,21 @@
     console.log(currentDate);
     console.log(releaseDate);
 </script>
+<html lang="en">
+    <body>
+        <div class="pokobox">
+            <h1>Days Until Pikmin 4</h1>
+            <p>{Math.round(numDaysLeft)}</p>
+        </div>
+        
+    </body>
+</html>
 
-<h1>Days Until Pikmin 4</h1>
-<p>{Math.round(numDaysLeft)}</p>
+
 <!-- <p>That's around ~{Math.round(numHoursLeft)} Hours</p>
 <p>Or ~{Math.round(numMinutesLeft)} Minutes</p>
 <p>Or ~{Math.round(numSecondsLeft)} Seconds</p>
 <p>Or exactly {numMillisecondsLeft} Milliseconds</p> -->
+<style lang="scss">
+
+</style>
