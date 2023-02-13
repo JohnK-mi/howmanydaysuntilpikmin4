4a8b98af35f7bd6a8a521e416641ea294b682eb7
John Kirchner <j_kirchner@ucsb.edu>
2023-02-12 20:10:07 -0800

    Initial version of date acquirement and calculation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let currentDate = new Date();
+    let releaseDate = new Date('July 21, 2023 00:00:00');
+    let numMillisecondsLeft = releaseDate.getTime()-currentDate.getTime(); 
+    let numSecondsLeft = numMillisecondsLeft/1000;
+    let numMinutesLeft = numSecondsLeft/60;
+    let numHoursLeft = numMinutesLeft/60;
+    let numDaysLeft = numHoursLeft/24;
+    console.log(currentDate);
+    console.log(releaseDate);
+</script>
+
+<h1>Days Until Pikmin 4</h1>
+<p>{Math.round(numDaysLeft)}</p>
+<!-- <p>That's around ~{Math.round(numHoursLeft)} Hours</p>
+<p>Or ~{Math.round(numMinutesLeft)} Minutes</p>
+<p>Or ~{Math.round(numSecondsLeft)} Seconds</p>
+<p>Or exactly {numMillisecondsLeft} Milliseconds</p> -->
