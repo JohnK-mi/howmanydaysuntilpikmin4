50a3739411485c910b6a1b1ddfd54add811f1abe
J Kirchner <j_kirchner@ucsb.edu>
2023-02-13 22:09:15 -0800

    All assets except digits are now successfully imported and included in build

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,33 @@
 <script>
     export let data;
+    //importing digits so as to please vite or whatever demigod is preventing
+    //these assets from being imported and/or found
+    //Better solution likely exists, but this works for now
+    import zero from './../lib/0.png';
+    import one from '$lib/1.png';
+    import two from './../lib/2.png';
+    import three from './../lib/3.png';
+    import four from './../lib/4.png';
+    import five from './../lib/5.png';
+    import six from './../lib/6.png';
+    import seven from './../lib/7.png';
+    import eight from './../lib/8.png';
+    import nine from './../lib/9.png';
     //console.log(data);
 </script>
 
 <div class="poko-header">
-    <img class="poko-header-text" src="/header.png" alt="How many days until Pikmin 4?">
+    <img class="poko-header-text" src="./../lib/header.png" alt="How many days until Pikmin 4?">
 </div>
 <div class="digits">
     {#each data.digits as digit}
         <div class="poko-digit">
-            <img class="poko-digit-text" src="/{digit}.png" alt="{digit}" width=48 height=52>
+            <img class="poko-digit-text" src="$lib/{digit}.png" alt="{digit}" width=48 height=52>
         </div>
     {/each}
 </div>
 <div class="poko-footer">
-    <a href="https://www.buymeacoffee.com/jkirchner"><img class="poko-footer-image" src="/coffee.png" alt="Buy me a coffee?"></a>
+    <a href="https://www.buymeacoffee.com/jkirchner"><img class="poko-footer-image" src="./../lib/coffee.png" alt="Buy me a coffee?"></a>
 </div>
 
 <!-- <p>That's around ~{Math.round(numHoursLeft)} Hours</p>
--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,16 @@
 import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
+import importAssets from 'svelte-preprocess-import-assets';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess({
+	preprocess: [preprocess({
 		scss: {
 			prependData: `@import 'src/lib/global.scss';`
 		}
 	}),
+	importAssets()
+	],
 	kit: {
 		adapter: adapter()
 	}
